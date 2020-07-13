17ff7ca9bee113b6ed14b63b4a588f5c5b1bb075
Tobias Derksen <t.derksen@mailbox.org>
2020-07-13 13:25:27 +0200

    Initial commit

---
+++ src/test/java/de/codecentric/metricsdemo/MetricsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.codecentric.metricsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
